fa8bd15969527d468e760f291a106a62a300ec08
Guillaume Petiot <guillpetiot@gmail.com>
2018-06-19 21:48:21 +0200

    more devices

--- src/config.ml
+++ src/config.ml
@@ -2,7 +2,11 @@ open Mirage
 
 
 let main =
-  foreign "Unikernel.Main" job
+  foreign "Unikernel.Main" (block @-> console @-> fs @-> time @-> job)
 
+let block_impl = block_of_file "disk.img"
+let fs_impl = fat block_impl
+    
 let () =
-  register "my_app" [main]
+  register "mirage-shell" [main $ block_impl $ default_console $ fs_impl $
+			      default_time]
--- src/unikernel.ml
+++ src/unikernel.ml
@@ -1,7 +1,11 @@
 
-module Main = struct
+module Main
+  (B: Mirage_types_lwt.BLOCK)
+  (C: Mirage_types_lwt.CONSOLE)
+  (FS: Mirage_types_lwt.FS)
+  (T: Mirage_types_lwt.TIME) = struct
 
-  let start =
+  let start _b _c _fs _t =
     Lwt.return_unit
       
 end
